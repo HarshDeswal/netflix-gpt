40910c3ae79bcb1c880556cf37a3074655024e74
harshdeswal <harshdeswal03@yahoo.com>
2024-01-03 22:56:28 +0530

    firebase sign in/sign up done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,56 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Header from './Header';
+import { createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+import { checkValidData } from '../utils/validate';
+import { auth } from '../utils/firebase';
 
 const Login = () => {
   const [isSignInForm,setIsSignForm] = useState(true);
+  const [errorMessage,setErrorMessage] = useState(null); 
+  const email = useRef(null);
+  const password = useRef(null);
   const toggleChange = () => {
     setIsSignForm(!isSignInForm); 
+  };
+  const handleButtonClick = () => {
+    //validate the form data
+    const message = checkValidData(email.current.value,password.current.value);
+    setErrorMessage(message);
+    if(message) return ;
+    if(!isSignInForm){ // Sign up
+      createUserWithEmailAndPassword(
+        auth, 
+        email.current.value, 
+        password.current.value)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    console.log(user);
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    setErrorMessage(errorCode + "-" + errorMessage);
+    // ..
+  });
+
+    }
+    else{
+      //sign in
+      signInWithEmailAndPassword(auth,email.current.value, 
+        password.current.value)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    setErrorMessage(errorCode+"-"+errorMessage);
+  });
+    }
   };
   return (
     <div>
@@ -12,12 +58,15 @@ const Login = () => {
         <div className='absolute'>
             <img src='https://assets.nflxext.com/ffe/siteui/vlv3/c31c3123-3df7-4359-8b8c-475bd2d9925d/15feb590-3d73-45e9-9e4a-2eb334c83921/IN-en-20231225-popsignuptwoweeks-perspective_alpha_website_large.jpg' />
         </div>
-        <form className=' w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
+        <form 
+        onSubmit={(e)=> e.preventDefault()}
+        className=' w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
             <h1 className='font-bold text-3xl py-4'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
             {!isSignInForm && <input type='text' placeholder='Full Name' className='p-4 my-4 w-full bg-gray-800'/>}
-            <input type='text' placeholder='Email Address' className='p-4 my-4 w-full bg-gray-800'/>
-            <input type='password' placeholder='Password' className='p-4 my-4 w-full bg-gray-800'/>
-            <button className='p-4 my-6 bg-red-600 w-full rounded-lg'>{isSignInForm ? "Sign In" : "Sign Up"}</button>
+            <input ref={email} type='text' placeholder='Email Address' className='p-4 my-4 w-full bg-gray-800'/>
+            <input ref={password} type='password' placeholder='Password' className='p-4 my-4 w-full bg-gray-800'/>
+            <p className='text-red-700 font-bold text-xl'>{errorMessage}</p>
+            <button className='p-4 my-6 bg-red-600 w-full rounded-lg' onClick={handleButtonClick}>{isSignInForm ? "Sign In" : "Sign Up"}</button>
             <p className='py-4 cursor-pointer' onClick={toggleChange}>
             {isSignInForm ? "New to Netflix? Sign Up" : "Already registered? Sign Up Now..."}
             </p>
---
+++ src/utils/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDD25c86lGPBj_pcq8rmmoWsDVlUwAPT5Y",
+  authDomain: "netflixgpt-46d0a.firebaseapp.com",
+  projectId: "netflixgpt-46d0a",
+  storageBucket: "netflixgpt-46d0a.appspot.com",
+  messagingSenderId: "875884764208",
+  appId: "1:875884764208:web:13b1e7ce5cbf429fa1e1f5",
+  measurementId: "G-0463QMC6Y9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth= getAuth();
