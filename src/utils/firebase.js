926a86403d3cedff080ac515a8f1e3967b34bf89
harshdeswal <harshdeswal03@yahoo.com>
2024-01-16 00:04:33 +0530

    TMDB API data fetch && built main container

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: "AIzaSyDD25c86lGPBj_pcq8rmmoWsDVlUwAPT5Y",
   authDomain: "netflixgpt-46d0a.firebaseapp.com",
   projectId: "netflixgpt-46d0a",
   storageBucket: "netflixgpt-46d0a.appspot.com",
